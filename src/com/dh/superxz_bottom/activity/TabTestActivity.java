c9252ba98dfef91e1fb73f314dac5597ebdefe47
yyy <1291883194@qq.com>
2017-01-05 15:32:54 +0800

    tab viewpager

--- src/com/dh/superxz_bottom/activity/TabTestActivity.java
+++ src/com/dh/superxz_bottom/activity/TabTestActivity.java
@@ -3,7 +3,7 @@ package com.dh.superxz_bottom.activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.widget.RadioButton;
+import android.support.v4.view.ViewPager;
 import android.widget.RadioGroup;
 
 import com.dh.superxz_bottom.R;
@@ -12,7 +12,7 @@ import com.dh.superxz_bottom.fragment.BitmapFragment;
 import com.dh.superxz_bottom.fragment.EventBusFragment;
 import com.dh.superxz_bottom.xutils.view.ViewUtils;
 import com.dh.superxz_bottom.xutils.view.annotation.ViewInject;
-import com.dh.superxz_bottom.yinzldemo.VehicleActivity;
+import com.dh.superxz_bottom.yinzldemo.VehicleNoSwipbackActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,13 +21,12 @@ import java.util.List;
  * Created by Administrator on 2017/1/4.
  */
 
-public class TabTestActivity extends VehicleActivity implements FragmentTabUtils.OnRgsExtraCheckedChangedListener {
+public class TabTestActivity extends VehicleNoSwipbackActivity implements FragmentTabUtils.OnRgsExtraCheckedChangedListener {
     @ViewInject(R.id.rg_tab)
     private RadioGroup rg_tab;
-    @ViewInject(R.id.radioButton1)
-    private RadioButton radioButton1;
-    @ViewInject(R.id.radioButton2)
-    private RadioButton radioButton2;
+
+    @ViewInject(R.id.viewPager)
+    private ViewPager viewPager;
 
     List<Fragment> fragments = new ArrayList<Fragment>();
     FragmentTabUtils fragmentTabUtils;
@@ -47,7 +46,8 @@ public class TabTestActivity extends VehicleActivity implements FragmentTabUtils
     private void initview() {
         fragments.add(0,bitmapFragment );
         fragments.add(1,eventBusFragment);
-        fragmentTabUtils=new FragmentTabUtils(fragmentManager,fragments,R.id.fragmentContent,rg_tab,this );
+//        fragmentTabUtils=new FragmentTabUtils(fragmentManager,fragments,R.id.fragmentContent,rg_tab,this );
+        fragmentTabUtils=new FragmentTabUtils(viewPager,fragmentManager,fragments,rg_tab);
 
     }
 
--- src/com/dh/superxz_bottom/dhutils/FragmentTabUtils.java
+++ src/com/dh/superxz_bottom/dhutils/FragmentTabUtils.java
@@ -3,7 +3,10 @@ package com.dh.superxz_bottom.dhutils;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -17,6 +20,8 @@ public class FragmentTabUtils implements RadioGroup.OnCheckedChangeListener {
     private int fragmentContentId;
     private int currentTab;
     private OnRgsExtraCheckedChangedListener onRgsExtraCheckedChangedListener;
+    private ViewPager viewPager;
+
 
     public FragmentTabUtils(FragmentManager fragmentManager, List<Fragment> fragments, int fragmentContentId, RadioGroup rgs, OnRgsExtraCheckedChangedListener onRgsExtraCheckedChangedListener) {
         this.fragments = fragments;
@@ -28,28 +33,49 @@ public class FragmentTabUtils implements RadioGroup.OnCheckedChangeListener {
         ((RadioButton) rgs.getChildAt(0)).setChecked(true);
 
     }
+    public FragmentTabUtils(ViewPager viewPager,FragmentManager fragmentManager, List<Fragment> fragments, RadioGroup rgs) {
+        this.fragments = fragments;
+        this.rgs = rgs;
+        this.viewPager=viewPager;
+        this.fragmentManager = fragmentManager;
 
+
+        ((RadioButton) rgs.getChildAt(0)).setChecked(true);
+        for (int i = 0; i < rgs.getChildCount(); i++) {
+            ((RadioButton) rgs.getChildAt(i))
+                    .setOnClickListener(new MyOnClickListener(i));
+        }
+
+        viewPager.setAdapter(new MyFragmentAdapter(fragmentManager,
+                fragments));
+        viewPager.setCurrentItem(0);
+        viewPager.setOffscreenPageLimit(fragments.size());
+        viewPager.setOnPageChangeListener((ViewPager.OnPageChangeListener) new MyOnPageChangeListener());
+
+    }
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-        for (int i = 0; i < rgs.getChildCount(); i++) {
-            if (rgs.getChildAt(i).getId() == checkedId) {
-                Fragment fragment = fragments.get(i);
-                FragmentTransaction ft = obtainFragmentTransaction(i);
-                getCurrentFragment().onStop();
-                if (fragment.isAdded()) {
-                    fragment.onStart();
-                } else {
-                    ft.add(fragmentContentId, fragment, String.valueOf(i));
-                    ft.commit();
+            for (int i = 0; i < rgs.getChildCount(); i++) {
+                if (rgs.getChildAt(i).getId() == checkedId) {
+                    Fragment fragment = fragments.get(i);
+                    FragmentTransaction ft = obtainFragmentTransaction(i);
+                    getCurrentFragment().onStop();
+                    if (fragment.isAdded()) {
+                        fragment.onStart();
+                    } else {
+                        ft.add(fragmentContentId, fragment, String.valueOf(i));
+                        ft.commit();
+                    }
+                    showTab(i);
+                    if (null != onRgsExtraCheckedChangedListener) {
+                        onRgsExtraCheckedChangedListener.OnRgsExtraCheckedChanged(radioGroup, checkedId, i);
+                    }
                 }
-                showTab(i);
-                if (null != onRgsExtraCheckedChangedListener) {
-                    onRgsExtraCheckedChangedListener.OnRgsExtraCheckedChanged(radioGroup, checkedId, i);
-                }
-            }
         }
 
     }
+
+
 
     private void showTab(int idx) {
         for (int i = 0; i < fragments.size(); i++) {
@@ -93,4 +119,64 @@ public class FragmentTabUtils implements RadioGroup.OnCheckedChangeListener {
     public static interface OnRgsExtraCheckedChangedListener {
         public void OnRgsExtraCheckedChanged(RadioGroup radioGroup, int checkedId, int index);
     }
+
+
+     class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
+
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        public void onPageSelected(int arg0) {
+            for (int i = 0; i < rgs.getChildCount(); i++) {
+            if (i== arg0) {
+                ((RadioButton) rgs.getChildAt(i)).setChecked(true);
+            }
+
+            }
+
+        }
+
+    }
+    class MyOnClickListener implements View.OnClickListener {
+        private int index = 0;
+
+        public MyOnClickListener(int i) {
+            index = i;
+        }
+
+        public void onClick(View v) {
+            viewPager.setCurrentItem(index);
+
+        }
+
+    }
+
+
+     class MyFragmentAdapter extends FragmentPagerAdapter {
+        List<Fragment> frags;
+
+        public MyFragmentAdapter(FragmentManager fm, List<Fragment> frag) {
+            super(fm);
+            this.frags = frag;
+        }
+
+        @Override
+        public Fragment getItem(int arg0) {
+            return frags.get(arg0);
+        }
+
+        @Override
+        public int getCount() {
+            return frags.size();
+        }
+
+    }
+
+
+
 }
